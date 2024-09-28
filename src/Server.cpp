215fb9c15f5f7f7062f035097fb4877d430b5507
Harshit Gupta <57803023+H4R5H1T-007@users.noreply.github.com>
2024-09-28 20:24:23 +0530

    Handle concurrent clients - debug

--- src/Server.cpp
+++ src/Server.cpp
@@ -97,6 +97,7 @@ int asyncAccept(int server_fd, sockaddr *, socklen_t *){
     int client_addr_len = sizeof(client_addr);
     int clientSocket = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
     {
+      std::cout<<"Async Thread\n";
       std::lock_guard<std::mutex> queue_lock(client_queue_lock);
       client_queue.push(clientSocket);
     }
