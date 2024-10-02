6075854fe20c84a9b947ee7555a8a255cd7cd5cb
Harshit Gupta <57803023+H4R5H1T-007@users.noreply.github.com>
2024-10-02 17:00:30 +0530

    Handle concurrent clients - works on my machine.

--- src/Server.cpp
+++ src/Server.cpp
@@ -48,7 +48,7 @@ int main(int argc, char **argv) {
   server_addr.sin_port = htons(6379);
   
   if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
-    std::cerr << "Failed to bind to port 6379\n";
+    std::cerr << "Failed to bind to port 637\n";
     perror("bind failed. Error");
     return 1;
   }
