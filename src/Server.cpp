8d3827bc0ca7de11c748b70993c330a3aabf500b
Harshit Gupta <57803023+H4R5H1T-007@users.noreply.github.com>
2024-10-02 16:16:02 +0530

    Handle concurrent clients - works on my machine.

--- src/Server.cpp
+++ src/Server.cpp
@@ -83,7 +83,7 @@ int main(int argc, char **argv) {
       std::vector<std::future<void>> task_list;
 
       for (int i = 0; i < fds.size(); ) {
-          std::cout<<"file desc is "<<fds[i].fd<<" POLLIn is "<<POLLIN<<"\n";
+          std::cout<<"file desc is "<<fds[i].fd<<" POLLIn is "<<POLLIN<<" server_fd is "<<server_fd<<"\n";
           if ((fds[i]).revents & POLLIN && fds[i].fd != -1) {
               if (fds[i].fd == server_fd) {
                   // New connection
